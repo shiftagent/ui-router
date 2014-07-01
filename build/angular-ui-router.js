7b96ee8879aa6d33f2d4ecf13e610b81423d2b4f
Matt Webb <matt@shiftagent.org>
2014-07-01 09:39:56 -0400

    build for 0.3.0prelim

--- build/angular-ui-router.js
+++ build/angular-ui-router.js
@@ -1,6 +1,6 @@
 /**
  * State-based routing for AngularJS
- * @version v0.2.10-dev-2014-06-16
+ * @version v0.2.10-dev-2014-07-01
  * @link http://angular-ui.github.com/
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
